f6b6bc7b9b3770a3e8fec738a1101e7cc7f16d16
Helbert Garcia <helbert1001@gmail.com>
2025-11-24 16:27:20 -0400

    Creando el modulo de "Customers", se creo el servicio y el controlador y ya esta funcional

---
+++ SGP.Services/DTO/Customer/AddCustomerDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SGP.Services.DTO.Customer
+{
+    public class AddCustomerDto
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio")]
+        [EmailAddress(ErrorMessage = "El correo electrónico no es válido")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "El número de teléfono es obligatorio")]
+        [Phone(ErrorMessage = "El número de teléfono no es válido")]
+        public string? Phone { get; set; }
+    }
+}
