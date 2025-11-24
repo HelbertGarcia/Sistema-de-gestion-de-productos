1a5a0e807aa51920c4c251327d0f6f9d1a79497a
Helbert Garcia <helbert1001@gmail.com>
2025-11-24 17:50:37 -0400

    Generando modulo Invoice

---
+++ SGP.Services/DTO/Invoice/AddInvoiceDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SGP.Services.DTO.Invoice
+{
+    public class AddInvoiceDto
+    {
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "La factura debe tener al menos un producto")]
+        public List<AddInvoiceDetailDto>? Items { get; set; }
+    }
+}
---
+++ SGP.Services/DTO/Invoice/InvoiceDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGP.Services.DTO.Invoice
+{
+    public class InvoiceDetailDto
+    {
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; } // Extra útil
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal SubTotal => Quantity * UnitPrice;
+    }
+}
